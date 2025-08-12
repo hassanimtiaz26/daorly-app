6bfde67136d95c85eb9f0db5bef6eab40c27a7a8
devsyncio <devsync.io@gmail.com>
2025-08-12 12:55:39 +0400

    initial

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,35 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet, TextInput } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { Input } from 'postcss';
+
+export default function HomeScreen() {
+  return (
+    <ThemedView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <TextInput style={{ width: '100%' }} placeholder={'Enter Name'} />
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
